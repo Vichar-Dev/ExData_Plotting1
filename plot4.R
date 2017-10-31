96dbe255bc2bc76448c138f19afb61363e53b8d1
Vipul Sharma <developers@onedotos.com>
2017-10-31 11:21:52 +0530

    no message

--- plot4.R
+++ plot4.R
@@ -49,7 +49,8 @@ plot4 <- function(){
   ## Format dateTime Column
   t$dateTime <- as.POSIXct(dateTime)
   
-  par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+  par(mfrow=c(2,2))
+  
   with(t, {
     plot(Global_active_power~dateTime, type="l", 
          ylab="Global Active Power (kilowatts)", xlab="")
@@ -57,10 +58,10 @@ plot4 <- function(){
          ylab="Voltage (volt)", xlab="")
     plot(Sub_metering_1~dateTime, type="l", 
          ylab="Global Active Power (kilowatts)", xlab="")
-    lines(Sub_metering_2~dateTime,col='Red')
-    lines(Sub_metering_3~dateTime,col='Blue')
-    legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
-           legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+    lines(dateTime, Sub_metering_1)
+    lines(dateTime, Sub_metering_2, col = "red")
+    lines(dateTime, Sub_metering_3, col = "blue")
+    #legend("topright", names(t)[7:9], col = c("black", "red", "blue"), lty = c(1, 1, 1))
     plot(Global_reactive_power~dateTime, type="l", 
          ylab="Global Rective Power (kilowatts)",xlab="")
   })
